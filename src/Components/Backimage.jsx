9900dde1217d826c2f6cbdded5943ec5d514c2a7
Arunkumar1321 <akmirror619@gmail.com>
2026-05-26 10:55:46 +0530

    changes according to vercel screen size for sign in page

--- src/Components/Backimage.jsx
+++ src/Components/Backimage.jsx
@@ -1,6 +1,6 @@
 import bg from "../asset/Netflix_bg.jpg"
 const Backimage = () => {
-          <div className="fixed inset-0 -z-20">
+          <div className="fixed inset-0 -z-10">
             <img src={bg} alt="Netflix Background" className="w-full h-full object-cover" />
         </div>}
 export default Backimage;
