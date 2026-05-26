6324ef6fddf35c8ec1e2ec151e0fdd34b172f76c
Arunkumar1321 <akmirror619@gmail.com>
2026-05-26 11:51:50 +0530

    changes according to vercel screen size for sign in page

--- src/Components/Overlay.jsx
+++ src/Components/Overlay.jsx
@@ -1,6 +1,6 @@
 const Overlay = () => {
     return (
-       <div className="fixed bg-black/70 w-full h-full z-0  "></div>
+       <div className="fixed bg-black/70 w-full h-full z-10 "></div>
     );
 };
 
